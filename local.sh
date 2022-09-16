99f05c96348104f08363fd72cb761961514c6746
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2022-09-16 17:25:06 -0300

    wip

--- local.sh
+++ local.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-echo "running node";
-anvil;
-
 echo "deploying contracts";
 forge script ./script/Deploy.s.sol --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast
 
