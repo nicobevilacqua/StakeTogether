b68a349de6239217a6a1114f30181695fa7449ee
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2022-09-16 12:06:33 -0300

    wip

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,11 +2,63 @@
 pragma solidity ^0.8.13;
 
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
-contract Vault is Initializable {
+contract Vault is Initializable, ERC20, ERC20Burnable, ReentrancyGuard {
+    uint256 public immutable vaultAmount;
     uint256 public vaultId;
 
-    function initialize(uint256 _vaultId) external initializer {
+    address public immutable manager;
+
+    bool public locked;
+
+    struct Investor {
+        address user;
+        uint256 investment;
+    }
+
+    event FundsAdded(address indexed investor, uint256 amount, uint256 timestamp);
+    event FundsRetired(address indexed investor, uint256 amount, uint256 timestamp);
+
+    constructor(uint256 _vaultAmount, address _manager) ERC20("StakeToguetherToken", "STT") {
+        require(_vaultAmount > 0, "Invalid vaultAmount");
+        vaultAmount = _vaultAmount;
+        manager = _manager;
+    }
+
+    function initialize(uint256 _vaultId) external payable initializer {
         vaultId = _vaultId;
+    }
+
+    function addFunds() external payable {
+        require(msg.value > 0, "invalid investment");
+        require(!locked, "vault locked");
+
+        uint256 fundsToReceive = Math.min(vaultAmount - totalSupply(), msg.value);
+        require(fundsToReceive > 0, "invalid funds");
+
+        _mint(msg.sender, fundsToReceive);
+
+        if (fundsToReceive < msg.value) {
+            Address.sendValue(payable(msg.sender), msg.value - fundsToReceive);
+            locked = true;
+        }
+
+        emit FundsAdded(msg.sender, fundsToReceive, block.timestamp);
+    }
+
+    function retireFunds(uint256 _amount) external nonReentrant {
+        require(balanceOf(msg.sender) > _amount, "invalid amount");
+        require(!locked, "vault locked");
+
+        Address.sendValue(payable(msg.sender), _amount);
+
+        _burn(msg.sender, _amount);
+
+        emit FundsRetired(msg.sender, _amount, block.timestamp);
     }
 }
--- src/VaultManager.sol
+++ src/VaultManager.sol
@@ -5,32 +5,63 @@ import {Vault} from "./Vault.sol";
 import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
 contract VaultManager {
     using Counters for Counters.Counter;
     Counters.Counter internal _vaultId;
 
     uint256 public constant VAULT_AMOUNT = 32 ether;
+    address public immutable vaultImplementation;
 
-    address public immutable vault;
+    address _nextVault;
 
-    mapping(uint256 => address) public vaults;
+    mapping(uint256 => address[]) public vaults;
+    mapping(address => uint256[]) public userVaults;
+
+    event FundsAdded(address indexed user, uint256 amount, uint256 timestamp);
+    event VaultCreated(address indexed vault, uint256 timestamp);
 
     constructor() {
-        vault = address(new Vault());
+        vaultImplementation = address(new Vault(VAULT_AMOUNT), address(this));
     }
 
     function addFunds() external payable {
         require(msg.value > 0, "insufficient funds");
+
+        uint256 userPendingFunds = msg.value;
+        while (userPendingFunds > 0) {
+            uint256 nextVaultInvestmentAmount = Math.min(
+                VAULT_AMOUNT - nextVaultBalance,
+                userPendingFunds
+            );
+
+            nextVaultInvestors.push(Investor(msg.sender, nextVaultInvestmentAmount));
+
+            if (nextVaultBalance == VAULT_AMOUNT) {
+                _createNextVault();
+            }
+
+            userPendingFunds -= nextVaultInvestmentAmount;
+        }
+
+        emit FundsAdded(msg.sender, msg.value, block.timestamp);
     }
 
-    function createVault() private {
-        address newVault = Clones.clone(vault);
+    function _createNextVault() private {
+        address newVaultImplementation = Clones.clone(vaultImplementation);
 
         uint256 vaultId = _vaultId.current();
         _vaultId.increment();
 
-        Vault(newVault).initialize(vaultId);
+        Investor[] memory _investors = nextVaultInvestors;
 
-        vaults[vaultId] = newVault;
+        address[] vaultInvestors = Vault(newVaultImplementation).initialize(vaultId, _investors);
+
+        nextVaultBalance = 0;
+        delete nextVaultInvestors;
+
+        emit VaultCreated(newVaultImplementation, block.timestamp);
     }
 }
