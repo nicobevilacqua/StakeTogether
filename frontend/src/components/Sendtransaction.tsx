34bad0f56826685e78566e386b0279f2b29ff8b7
eugenioclrc <eugenioclrc@gmail.com>
2022-09-16 21:28:10 -0300

    agrego snipet

--- frontend/src/components/Sendtransaction.tsx
+++ frontend/src/components/Sendtransaction.tsx
@@ -41,3 +41,49 @@ export function SendTransaction() {
     </form>
   );
 }
+
+// esto es para enviar tx a contrato
+/*
+import * as React from 'react'
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from 'wagmi'
+
+export function MintNFT() {
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+  } = usePrepareContractWrite({
+    addressOrName: '0xaf0326d92b97df1221759476b072abfd8084f9be',
+    contractInterface: ['function mint()'],
+    functionName: 'mint',
+  })
+  const { data, error, isError, write } = useContractWrite(config)
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+  return (
+    <div>
+      <button disabled={!write || isLoading} onClick={() => write()}>
+        {isLoading ? 'Minting...' : 'Mint'}
+      </button>
+      {isSuccess && (
+        <div>
+          Successfully minted your NFT!
+          <div>
+            <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+          </div>
+        </div>
+      )}
+      {(isPrepareError || isError) && (
+        <div>Error: {(prepareError || error)?.message}</div>
+      )}
+    </div>
+  )
+}
+*/
