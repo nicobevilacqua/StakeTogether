46b9c1781b980d8bf9c13d8a2c05f2adb03ae6d5
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2022-09-22 21:34:32 -0300

    move time using bash

--- script/bash/3.time.js
+++
@@ -1,9 +0,0 @@
-const { ethers, network } = require('hardhat');
-
-// 6 meses
-async function main() {
-  await network.provider.send('evm_increaseTime', [6 * 24 * 30 * 60 * 60]);
-  await network.provider.send('evm_mine', []);
-}
-
-main().then(() => { console.log("READY") });
--- script/bash/3.time.sh
+++ script/bash/3.time.sh
@@ -2,4 +2,10 @@
 
 source ../../.env;
 
-npx hardhat run ./3.time.js --network localhost
+curl -H "Content-Type: application/json" -X POST --data \
+        '{"id":1337,"jsonrpc":"2.0","method":"evm_increaseTime","params":[15552000]}' \
+        http://127.0.0.1:8545
+
+curl -H "Content-Type: application/json" -X POST --data \
+        '{"id":1337,"jsonrpc":"2.0","method":"evm_mine","params":[]}' \
+        http://127.0.0.1:8545
